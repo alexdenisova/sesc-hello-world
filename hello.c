b9d72223be5cf2bfedde9c22dfb5dcab07c55cf5
alexdenisova <37692145+alexdenisova@users.noreply.github.com>
2018-09-20 20:10:42 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
   char buf[100], str[100];
-	printf("Enter name: ");
+	//printf("Enter name: ");
   while (fgets(buf, sizeof buf, stdin) != NULL) {
     if (sscanf(buf, "%s", &str) != 1) {
       printf("Hello, nameless!\n");
